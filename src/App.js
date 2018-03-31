cacc57b29ad655c701acbdf75216b3098b7b44a0
ivanzamora <faucar93@gmail.com>
2018-03-30 22:44:14 -0500

    Added new components Card and CardList with git avatars

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CardList from './Components/CardList';
+
+let peopleArray = [
+    
+  {
+      name:"Ivan Zamora Arias",
+      avatar_url:"https://avatars2.githubusercontent.com/u/30245989?v=4",
+      company:"IOET"
+  },
+  {
+      name:"Merd Mansourifar", 
+      avatar_url:"https://avatars0.githubusercontent.com/u/25105474?v=4", 
+      company:"IOET"
+  }
+];
 
 class App extends Component {
   render() {
@@ -8,11 +23,12 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Warm Welcome to ReactGame by Ivan Zamora Arias</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Say Hello :D 
         </p>
+        <CardList cards={peopleArray}/>
       </div>
     );
   }
